fb331fcda87d2c4c8aca223575cd09b5cec478d3
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-03-14 19:54:53 -0400

    set proxy to localhost:3001(backend)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    fetch()
+  }
   render() {
     return (
       <div className="App">
